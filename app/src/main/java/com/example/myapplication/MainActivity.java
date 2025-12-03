2a051e05aa281cc0a1fb1dd4be3063df2faae7cf
Gal <galcol2009@gmail.com>
2025-12-03 19:34:43 +0200

    משימת כיתה 3.12

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,24 +1,48 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button biggerBtn, equalBtn, smallerBtn, NumGenBtn;
+    TextView resultTv, num1Tv, num2Tv;
+    int num1, num2;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        resultTv = findViewById(R.id.resultTv);
+        num1Tv = findViewById(R.id.num1Tv);
+        num2Tv = findViewById(R.id.num2Tv);
+
+        biggerBtn = findViewById(R.id.biggerBtn);
+        equalBtn = findViewById(R.id.equalBtn);
+        smallerBtn = findViewById(R.id.smallerBtn);
+        NumGenBtn = findViewById(R.id.NumGenBtn);
+
+    }
+    public void onClickGenNums(android.view.View NumGenBtn) {
+        num1 = (int) Math.random()*101;
+        num2 = (int) Math.random()*101;
+        num1Tv.setText(""  + num1);
+        num2Tv.setText(""  + num2);
+    }
+    public void onClickCheckSm(android.view.View smallerBtn) {
+        resultTv.setText("" + (num1<num2));
+    }
+    public void onClickCheckEq(android.view.View equalBtn) {
+        resultTv.setText("" + (num1==num2));
+    }
+    public void onClickCheckBg(android.view.View biggerBtn) {
+        resultTv.setText("" + (num1>num2));
     }
 }
+
